3288241296aa8f8299404ac95ed97eb6dfadbad3
‘Kristina <kbecvar@umass.edu>
2021-07-10 16:33:37 -0500

    Chapter 4

---
+++ textbook.R
@@ -0,0 +1,17 @@
+1 / 200 * 30
+(59 + 73 + 2) / 3
+sin(pi / 2)
+x <- 3 * 4
+object_name <- value
+x
+this_is_a_really_long_name <- 2.5
+y <- seq(1, 10, length.out = 5)
+y
+#> [1]  1.00  3.25  5.50  7.75 10.00
+library(tidyverse)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+library(tidyverse)
+
+filter(mpg,cyl = 8)
