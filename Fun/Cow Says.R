57ef4ebe6852c1385ae36f27da9f4df4d149100b
Kristina <kbecvar@umass.edu>
2021-07-14 19:22:49 -0500

    Cleanup

--- Fun/Cow Says.R
+++
@@ -1,8 +0,0 @@
-library(cowsay)
-library(wikifacts)
-library(magrittr)
-wiki_randomfact(
-  n_facts = 1L,
-  fact = c("any", "didyouknow", "onthisday", "inthenews"),
-  bare_fact = FALSE,
-  repeats = TRUE) %>% say(by = "cow")
